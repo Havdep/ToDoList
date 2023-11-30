c520e9236fb1626afca6b933ae24dc507769bd1b
Nipun <nipunsingh29@hotmail.com>
2023-11-30 12:23:18 +0530

    Update 02

--- src/Components/TodoWrap.jsx
+++ src/Components/TodoWrap.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import TodoForm from "./todoForm";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoWrap() {
   return (
     <>
+      <Todo />
       <TodoForm />
     </>
   );
